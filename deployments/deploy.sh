3a243e90fdcff993c15885826f9acb359a07f061
cocm1324 <cocm1324@gmail.com>
2022-03-21 18:30:26 +0900

    renewall deploy script

--- deployments/deploy.sh
+++ deployments/deploy.sh
@@ -1,6 +1,10 @@
-docker build -f deployments/dockerfile -t ${DOCKER_IMAGE_TAG}:latest .
+NAME=${git config --get remote.origin.url | sed 's/.*\/\([^ ]*\/[^.]*\).*/\1/'}
+TAG=latest
+EXPOSE=3000
 
-docker ps -f name=${DOCKER_CONTAINER_NAME} -q | xargs --no-run-if-empty docker container stop
-docker container ls -a -fname=${DOCKER_CONTAINER_NAME} -q | xargs -r docker container rm
+docker build -f deployments/dockerfile -t ${NAME}:${TAG} .
+
+docker ps -f name=${NAME} -q | xargs --no-run-if-empty docker container stop
+docker container ls -a -fname=${NAME} -q | xargs -r docker container rm
 docker images --no-trunc --all --quiet --filter='dangling=true' | xargs --no-run-if-empty docker rmi
-docker run --network ${DOCKER_NETWORK_NAME} --expose 3000 -d --name ${DOCKER_CONTAINER_NAME} ${DOCKER_IMAGE_TAG}:latest
+docker run --network ${DOCKER_NETWORK} --expose ${EXPOSE} -d --name ${NAME} ${NAME}:${TAG}
